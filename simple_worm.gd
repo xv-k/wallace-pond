d409deb58e486765a856704a17b0fe97c0fdd6c4
xv-k <xv_k@outlook.com>
2026-04-29 21:20:05 +0200

    added movement parameters

--- simple_worm.gd
+++ simple_worm.gd
@@ -7,10 +7,15 @@ extends Node2D
 @onready var line = $Line2D
 
 var t = 0.0
-var segment_length = 50.0 # length of each tail segment
-var wave_amplitude = 1 # the max "up" and "down" movement of one node relative to node in front of it
-var wave_speed = 2 # the speed of the up and down movement
+@export_range(0,150,1,"Tail segment length","prefer_slider") var segment_length = 50.0 # length of each tail segment
+@export_range(0,5,0.1,"Wave amplitude","prefer_slider") var wave_amplitude = 2.0 # the max "up" and "down" movement of one node relative to node in front of it
+@export_range(0,3,0.1,"Wave speed","prefer_slider") var wave_speed = 2.0 # the speed of the up and down movement
 # an amplitude of 1 gives around 45° movement, 3.2 gives around 360°
+
+#extra parameters to tweak motion
+@export_range(0,2,0.1,"Phase delay","prefer_slider") var phase_delay = 0.5
+@export_range(0,1,0.1,"Amplitude diference invert factor","prefer_slider") var amplitude_difference_invert_factor = 1.0
+@export_range(0,3,0.1,"Amplitude difference factor","prefer_slider") var amplitude_difference_factor = 1.5
 
 var head_pos = Vector2(200, 240) # the position of the first node
 
@@ -28,10 +33,19 @@ func _process(delta: float) -> void:
 	var points = [A, B, C, D]
 
 	for i in range(1, points.size()):
-		var offset = i * 0.5  # phase delay (if offset is zero, then you have a straight stick)
+		var offset = i * phase_delay  # phase delay (if offset is zero, then you have a straight stick)
 		# offsets looks like the "stiffness" of the tail
 		
-		var angle = sin(t * wave_speed - offset) * wave_amplitude
+		var k = i / (float(points.size())-1) # to make the wave amplitute different in each tail section
+		# first node is 1/3 th of the amplitude, second node 1/2, third 1 full amplitude (in the case with a head + 3 nodes)
+		# you can also do the opposite 
+		
+		var falloff = lerp(1-k,k,amplitude_difference_invert_factor) * amplitude_difference_factor #to invert the wave amplitute difference between nodes
+		# (k makes movement increase from head to tail)
+		# (1-k makes movement decrease from head to tale)
+		# amplitude_difference_invert_factor lerps between the two
+		
+		var angle = sin(t * wave_speed - offset) * wave_amplitude * falloff
 		# - ofset looks the tail movement is starting at the head node and moving down to the last node
 		# + ofstet looks like the tail end starts the movement, going up towards the head (looks wrong)
 		
